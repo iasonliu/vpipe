df253f41dd5b8f11718ea721d8cfd9b33a8626f0
xiangxu <iasonliu@gmail.com>
2022-03-30 14:32:47 +0800

    add box reader and write

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
 use clap::Parser;
+use std::fs::File;
 use std::env;
 use std::io::{self, ErrorKind, Read, Result, Write};
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
-    #[clap(help = "Read from a file instead of stdin")]
+    #[clap(help = "Read from a file instead of stdin", default_value="")]
     infile: String,
     #[clap(
         short = 'o',
         long = "outfile",
         takes_value = true,
-        help = "Write to stdout instead of stdout"
+        help = "Write to stdout instead of stdout",
+        default_value = ""
     )]
     outfile: String,
     #[clap(short = 's', long = "silent", help = "silent output")]
@@ -20,17 +22,28 @@ struct Args {
 const CHUNK_SIZE: usize = 16 * 1024;
 fn main() -> Result<()> {
     let args = Args::parse();
-    let _infile = args.infile;
-    let _outfile = args.outfile;
+    let infile = args.infile;
+    let outfile = args.outfile;
     let silent = if args.silent {
         true
     } else {
         !env::var("PPVR_SILENT").unwrap_or_default().is_empty()
     };
+    let mut reader: Box<dyn Read> = if !infile.is_empty() {
+        Box::new(File::open(infile)?)
+    } else {
+        Box::new(io::stdin())
+    };
+
+    let mut writer: Box<dyn Write> = if !outfile.is_empty() {
+        Box::new(File::create(outfile)?)
+    } else {
+        Box::new(io::stdout())
+    };
     let mut total_bytes = 0;
     let mut buffer = [0; CHUNK_SIZE];
     loop {
-        let num_read = match io::stdin().read(&mut buffer) {
+        let num_read = match reader.read(&mut buffer) {
             Ok(0) => break,
             Ok(x) => x,
             Err(_) => break,
@@ -39,7 +52,7 @@ fn main() -> Result<()> {
         if !silent {
             eprint!("\r{}", total_bytes);
         }
-        if let Err(e) = io::stdout().write_all(&buffer[..num_read]) {
+        if let Err(e) = writer.write_all(&buffer[..num_read]) {
             match e.kind() {
                 ErrorKind::BrokenPipe => break,
                 _ => return Err(e),
