df4639464b470fd07ce93e561de607b346f3793d
xiangxu <iasonliu@gmail.com>
2022-03-31 08:43:04 +0800

    using crossbeam channel

--- src/stats.rs
+++ src/stats.rs
@@ -1,23 +1,14 @@
+use crossbeam::channel::Receiver;
 use std::io::Result;
-use std::sync::mpsc::{Receiver, Sender};
 
-pub fn stats_loop(
-    silent: bool,
-    stats_rx: Receiver<Vec<u8>>,
-    write_tx: Sender<Vec<u8>>,
-) -> Result<()> {
+pub fn stats_loop(silent: bool, stats_rx: Receiver<usize>) -> Result<()> {
     let mut total_bytes = 0;
     loop {
         // receive from read
-        let buffer = stats_rx.recv().unwrap();
-        let num_bytes = buffer.len();
+        let num_bytes = stats_rx.recv().unwrap();
         total_bytes += num_bytes;
         if !silent {
             eprint!("\r{}", total_bytes);
-        }
-        // send vector to write loop
-        if write_tx.send(buffer).is_err() {
-            break;
         }
         if num_bytes == 0 {
             break;
