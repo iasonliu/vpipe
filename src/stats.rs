a5de2d23e2a6867011981f94b1e8cce520132cea
xiangxu <iasonliu@gmail.com>
2022-04-01 08:11:07 +0800

    downgrade cossterm

--- src/stats.rs
+++ src/stats.rs
@@ -1,24 +1,29 @@
 use crossbeam::channel::Receiver;
-use std::io::Result;
+use crossterm::{
+    cursor, execute,
+    style::{self, Color, PrintStyledContent},
+    terminal::{Clear, ClearType},
+};
+use std::io::{self, Result, Stderr, Write};
 use std::time::{Duration, Instant};
 
 pub fn stats_loop(silent: bool, stats_rx: Receiver<usize>) -> Result<()> {
     let mut total_bytes = 0;
-    let mut timer = Timer::new();
     let start = Instant::now();
+    let mut timer = Timer::new();
+    let mut stderr = io::stderr();
     loop {
-        // receive from read
         let num_bytes = stats_rx.recv().unwrap();
         timer.update();
         let rate_per_second = num_bytes as f64 / timer.delta.as_secs_f64();
         total_bytes += num_bytes;
         if !silent && timer.ready {
             timer.ready = false;
-            eprint!(
-                "\r{} {} [{:.0}b/s]",
+            output_progress(
+                &mut stderr,
                 total_bytes,
                 start.elapsed().as_secs().as_time(),
-                rate_per_second
+                rate_per_second,
             );
         }
         if num_bytes == 0 {
@@ -29,6 +34,21 @@ pub fn stats_loop(silent: bool, stats_rx: Receiver<usize>) -> Result<()> {
         eprintln!();
     }
     Ok(())
+}
+
+fn output_progress(stderr: &mut Stderr, bytes: usize, elapsed: String, rate: f64) {
+    let bytes = style::style(format!("{} ", bytes)).with(Color::Red);
+    let elapsed = style::style(elapsed).with(Color::Green);
+    let rate = style::style(format!(" [{:.0}b/s]", rate)).with(Color::Blue);
+    let _ = execute!(
+        stderr,
+        cursor::MoveToColumn(0),
+        Clear(ClearType::CurrentLine),
+        PrintStyledContent(bytes),
+        PrintStyledContent(elapsed),
+        PrintStyledContent(rate)
+    );
+    let _ = stderr.flush();
 }
 
 trait TimerOutput {
